122af0edf571c447ec066a0aa0ab6cd00de37f78
Justin Crosby <justin.crosby@gmail.com>
2023-10-07 06:33:19 -0400

    adding domain named bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,3 +64,58 @@ resource "aws_s3_object" "index_file" {
   acl          = "public-read"        # Set ACL to make the object publicly readable
   etag         = filemd5("../site/index.html")
 }
+
+
+################
+
+
+resource "aws_s3_bucket" "links-sherman" {
+  bucket = "links.sherman.industries"
+}
+
+resource "aws_s3_bucket_ownership_controls" "links-sherman" {
+  bucket = aws_s3_bucket.links-sherman.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "links-sherman" {
+  bucket = aws_s3_bucket.links-sherman.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "links-sherman" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.links-sherman,
+    aws_s3_bucket_public_access_block.links-sherman,
+  ]
+
+  bucket = aws_s3_bucket.links-sherman.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "links-sherman" {
+  bucket = aws_s3_bucket.links-sherman.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+}
+
+resource "aws_s3_object" "index_file" {
+  bucket       = aws_s3_bucket.links-sherman.id
+  key          = "index.html"         # The desired object key (file name) in the bucket
+  source       = "../site/index.html" # Local path to your index.html file
+  content_type = "text/html"          # Specify the content type if needed
+  acl          = "public-read"        # Set ACL to make the object publicly readable
+  etag         = filemd5("../site/index.html")
+}
